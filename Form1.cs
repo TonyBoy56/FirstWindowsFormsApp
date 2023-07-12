559b1158d4dafade310f31ccfaa4c2c1be343524
Antonio Cruz <antonio.cruz5612@gmail.com>
2023-07-12 15:06:40 -0700

    add a simple message and Dialog box buttons to test the creation of a windows form.

---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string messageTitle = "Simple Message";
+            string messageText = "Hey man, I'm just writing some notes real quick over here. How would you feel about closing this window, maybe?";
+
+
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(messageText, messageTitle, buttons, MessageBoxIcon.Information);
+            //MessageBox.Show(messageText, messageTitle);
+
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else if (result == DialogResult.No)
+            {
+                return;
+            }
+        }
+    }
+}
